6862d44feb17251904159739edf5d8fa274b4d52
Antonio Fry <antoniofry2000@gmail.com>
2019-06-24 22:11:06 -0600

    add map method to create person component

--- src/components/Cohort.js
+++ src/components/Cohort.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Person from './Person';
 import './Cohort.css';
 
-const Cohort = () => {
+const Cohort = (props) => {
+  console.log(props);
+  const formatted = props.people.map(person => {
+    return <Person image={person.photo} name={person.name}
+    quote={person.quote} superlative={person.superlative} />
+  })
+  
   return (
     <main>
       <h2>Cohort</h2>
-      <Person />
+        {formatted}
     </main>
   )
 }
